4ce1c0987a69535de11389ac0953a1d4f511b43c
Lones <cjszxzx520@163.com>
2021-07-28 15:04:40 +0800

    feat:添加工程列表

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,37 @@
+import { SearchPanel } from "./serach-panel";
+import { List } from "./list";
+import { useEffect, useState } from "react";
+import React from "react";
+import { cleanObject } from "../../utils";
+import qs from "qs";
+const apiUrl = process.env.REACT_APP_API_URL;
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [users, setUsers] = useState([]);
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+      async (response) => {
+        if (response.ok) {
+          setList(await response.json());
+        }
+      }
+    );
+  }, [param]);
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, [param]);
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
